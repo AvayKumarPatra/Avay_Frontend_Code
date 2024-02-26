149ecb51010425ece61945879100dce73b39eee8
AvayKpatra <147364657+AvayKpatra@users.noreply.github.com>
2024-02-26 10:05:48 +0530

    Magento pwa project

---
+++ pwaSample/componentOverrideMapping.js
@@ -0,0 +1,4 @@
+const veniaUiPackagePath = '@magento/venia-ui';
+module.exports = componentOverrideMapping = {
+    [`${veniaUiPackagePath}/lib/components/Footer/footer.js`]: 'src/components/Footer/footer.js'
+};
