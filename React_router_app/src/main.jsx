e3b28f9b1bbf7c2e0bcbe1b9cb94270280554abf
AvayKpatra <147364657+AvayKpatra@users.noreply.github.com>
2024-03-04 10:10:06 +0530

    Modified router file

--- React_router_app/src/main.jsx
+++ React_router_app/src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Route from "./Route.jsx";
+import Home from "../src/components/Home/Home.jsx";
+import About from "../src/components/About/About.jsx";
+import Contact from "./components/Contacts/Contacts.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Route />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact-us",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
