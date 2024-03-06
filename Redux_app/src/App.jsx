e9ccf373ec88edc4eff4aec6604e846e7e4840c4
AvayKpatra <147364657+AvayKpatra@users.noreply.github.com>
2024-03-06 14:41:55 +0530

    Toggle theme code pushed

--- Redux_app/src/App.jsx
+++ Redux_app/src/App.jsx
@@ -1,19 +1,37 @@
-import Slider from "./Slider";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/theme";
+import { ThemeBtn } from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 const App = () => {
-  const images = [
-    "https://cdn.pixabay.com/photo/2017/02/12/18/26/squirrel-2060807_1280.jpg",
-    "https://media.istockphoto.com/id/1483708496/photo/a-man-working-at-an-auto-repair-shop.jpg?s=2048x2048&w=is&k=20&c=JO5kUqt2bTu8jPzIZlCEA-3P2IgaEvOxwJpy2_FtIAc=",
-    "https://cdn.pixabay.com/photo/2022/08/24/05/44/duck-7406987_1280.jpg",
-    "https://cdn.pixabay.com/photo/2017/02/12/18/26/squirrel-2060807_1280.jpg",
-  ];
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //  Actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <>
-      <h1 className="text-red-700">Carousel</h1>
-      <Slider images={images} />
-    </>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 };
-
 export default App;
