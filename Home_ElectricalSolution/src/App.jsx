c3608c41413e9d4a51f42acad66fc27585fffa9e
AvayKpatra <147364657+AvayKpatra@users.noreply.github.com>
2024-03-11 14:50:37 +0530

    Adding home electrical olution sample component

---
+++ Home_ElectricalSolution/src/App.jsx
@@ -0,0 +1,13 @@
+import HeaderScrollBar from "./components/HeaderScrollBar";
+import UserFeedback from "./components/UserFeedback";
+
+const App = () => {
+  return (
+    <>
+      <HeaderScrollBar />
+      {/* <UserFeedback /> */}
+    </>
+  );
+};
+
+export default App;
